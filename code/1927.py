011c1dfd795ff9c995d3991f3ba4e0834c091f4b
jaeyun95 <wodbs9522@gmail.com>
2021-05-09 00:45:42 +0900

    update

---
+++ code/1927.py
@@ -0,0 +1,16 @@
+#(1927) 최소 힙
+
+import sys
+import heapq
+
+N = int(sys.stdin.readline())
+heap = []
+for _ in range(N):
+    X = int(sys.stdin.readline())
+    if X == 0:
+        if len(heap) == 0: print(0)
+        else:
+            out = heapq.heappop(heap)[1]
+            print(out)
+    else:
+        heapq.heappush(heap,[X,X])
